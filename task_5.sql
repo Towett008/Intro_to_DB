80a7aa079d9e615e5552df77cefa37ae14ceeeeb
Cynthia Chepkorir <towettcynthia041@gmail.com>
2025-10-19 15:38:07 +0300

    Add task_5.sql to insert a single customer record into Customers table

---
+++ task_5.sql
@@ -0,0 +1,6 @@
+-- Use the alx_book_store
+USE alx_book_store
+
+--Insert a single row into the Cusrtomers table
+INSERT INTO Cusrtomers (Cusrtomer_id, customer_name, email, address)
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com,', '123 Happiness Ave.');
